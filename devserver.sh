7ce07c15d68b874e48a1cacce4e5873d3ac4c550
Ole <ole@vprsm.de>
2015-08-11 14:51:15 +0200

    Add development utilities

---
+++ devserver.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+PIDFILE=".devserver.pid"
+LAUNCHCMD="node --harmony src/automatik.js"
+
+start_server() {
+	$LAUNCHCMD &
+	echo $! > $PIDFILE
+}
+
+restart_server() {
+	kill $(< $PIDFILE)
+	start_server
+}
+
+[[ -e $PIDFILE ]] && restart_server || start_server
