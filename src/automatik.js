6b2cb4304b92e5586dc60a7fabe5e7ebed647c03
Ole <ole@vprsm.de>
2015-08-12 14:01:55 +0200

    Seperate box template

--- src/automatik.js
+++ src/automatik.js
@@ -3,16 +3,23 @@ var route = require("koa-route");
 var dot = require("dot");
 
 var tpl = dot.process({path: "src/views"});
+tpl.boxes = dot.process({path: "src/views/boxes"});
 var web = koa();
 
-var info = {
+const info = {
 	title: "automatik",
 	version: "0.0.0"
 };
 
 web.use(route.get("/", function*() {
 	this.response.body = tpl.index({
-		info: info
+		info: info,
+		tiles: [
+			tpl.boxes.temp({
+				label: "Living room",
+				value: "20°C"
+			})
+		]
 	});
 }));
 
