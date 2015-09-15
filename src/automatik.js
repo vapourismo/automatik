7ca69b12360926ff58f187de1af48ff8cbb67eda
Ole <ole@vprsm.de>
2015-09-16 00:49:01 +0200

    Add room internal example

--- src/automatik.js
+++ src/automatik.js
@@ -13,23 +13,43 @@ const info = {
 	version: "0.0.0"
 };
 
-web.get("/", function (req, res) {
-	db.query("SELECT short_name, name FROM rooms ORDER BY name ASC", function (err, result) {
+web.get("/", (req, res) => res.redirect("/rooms"));
+
+web.get("/rooms", function (req, res) {
+	db.query("SELECT id, name FROM rooms ORDER BY name ASC", function (err, result) {
 		if (err) {
 			// TODO: Handle query errors
 			res.status(500).json(err);
 		} else {
 			res.send(tpl.overview({
 				info: info,
-				major: ["<div class='container box'></div>", "<div class='container box'></div>"],
-				minor: result.rows.map(tpl.boxes.room)
+				major: ["<div class='box'></div>", "<div class='box'></div>"],
+				minor: result.rows.map(tpl.boxes.minor)
 			}));
 		}
 	});
 });
 
-web.get("/rooms/:shortname", function (req, res) {
-	db.query("SELECT ")
+web.get("/rooms/:id", function (req, res) {
+	db.query(
+		"SELECT id, name FROM rooms WHERE id = $1",
+		[req.params.id],
+		function (err, result) {
+			if (err) {
+				// TODO: Handle query errors
+				res.status(500).json(err);
+			} else if (result.rows[0]) {
+				var room = result.rows[0];
+
+				res.send(tpl.room({
+					info: info,
+					room: room
+				}));
+			} else {
+				res.status(404).end();
+			}
+		}
+	);
 });
 
 db.connect();
