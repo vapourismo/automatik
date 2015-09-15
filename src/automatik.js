985598aee24d892e8b085883d604350805490a46
Ole <ole@vprsm.de>
2015-09-15 16:34:03 +0200

    Transition to the Express framework

--- src/automatik.js
+++ src/automatik.js
@@ -1,28 +1,26 @@
-var koa = require("koa");
-var route = require("koa-route");
+var express = require("express");
 var dot = require("dot");
 
 var tpl = dot.process({path: "src/views"});
 tpl.boxes = dot.process({path: "src/views/boxes"});
-var web = koa();
+
+var web = express();
 
 const info = {
 	title: "automatik",
 	version: "0.0.0"
 };
 
-web.use(route.get("/", function*() {
-	this.response.body = tpl.index({
+web.get("/", function (req, res) {
+	res.send(tpl.index({
 		info: info,
 		tiles: [
-			tpl.boxes.default({
-				id: "temp_living_room",
+			tpl.boxes.link({
 				label: "Living room",
-				value: "20°C",
-				href: "/path/to/tile/contents"
+				href: "/room/living_room"
 			})
 		]
-	});
-}));
+	}));
+});
 
 web.listen(3001);
