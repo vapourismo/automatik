c693afe3c243b1f233d13bd0d74136e10e8665d5
Ole <ole@vprsm.de>
2015-09-24 16:32:47 +0200

    Add overlay and pop functions on client-side

--- static/room.js
+++ static/room.js
@@ -1,7 +1,40 @@
+function showPopup(contents) {
+	$("#overlay .popup .contents")[0].innerHTML = contents;
+	$("#overlay").css({visibility: "visible"}).animate({opacity: 1});
+}
+
+function hidePopup() {
+	$("#overlay").animate(
+		{opacity: 0},
+		function () {
+			$(this).css({visibility: "hidden"})
+		}
+	);
+}
+
 var sockio = io();
 
 sockio.on("update-entity", function (msg) {
 	$(".entity[data-id=" + msg.id + "] .value").each(function (idx, elem) {
 		elem.innerText = msg.value;
+	});
+});
+
+sockio.on("display-popup", showPopup);
+
+$(document).ready(function () {
+	$(".entity").each(function (idx, elem) {
+		var entityID = elem.getAttribute("data-id");
+		if (!entityID)
+			return;
+
+		$(elem).click(function () {
+			var contents = $(".entity[data-id=" + entityID + "] .label")[0].innerText;
+			showPopup(new Date().toString() + ": " + contents);
+		});
+	});
+
+	$(".popup .header .close").click(function () {
+		hidePopup();
 	});
 });
