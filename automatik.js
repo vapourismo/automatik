bcb3c7182408adcbc043d91513fd2bcaa8e79825
Ole <ole@vprsm.de>
2015-09-28 01:47:42 +0200

    Integrate room and entity instances seperately

--- automatik.js
+++ automatik.js
@@ -1,45 +1,53 @@
+var http = require("http");
+var Express = require("express");
+
 var tpls = require("./lib/templates");
 var backends = require("./lib/backends");
-
-var instances = require("./src/instances.js");
-var server = require("./src/server.js");
-
-backends.loadAll(function (_, datapoints) {
-	instances.loadInstances(datapoints, server.comm);
-});
+var entities = require("./lib/entities");
+var communication = require("./lib/communication");
 
 const info = {
 	title: "automatik",
 	version: "0.0.0"
 };
 
-server.express.get("/", function (req, res) {
-	var rooms = [];
+var app = Express();
+var web = http.Server(app);
+var comm = new communication.Communication(web, entities);
 
-	for (var id in instances.rooms) {
-		rooms.push(tpls.boxes.room(instances.rooms[id]));
-	}
+function setupServer() {
+	app.get("/", function (req, res) {
+		var rooms = [];
 
-	res.send(tpls.overview({
-		info: info,
-		rooms: rooms
-	}));
-});
+		for (var id in entities.rooms) {
+			rooms.push(tpls.boxes.room(entities.rooms[id]));
+		}
 
-server.express.get("/rooms", (req, res) => res.redirect("/"));
-
-server.express.get("/rooms/:id", function (req, res) {
-	if (req.params.id in instances.rooms) {
-		var room = instances.rooms[req.params.id];
-
-		res.send(tpls.room({
+		res.send(tpls.overview({
 			info: info,
-			room: room.id,
-			entities: room.entities.map(entity => entity.renderBox())
+			rooms: rooms
 		}));
-	} else {
-		res.status(404).send("No room with ID found");
-	}
-});
+	});
 
-server.http.listen(3001);
+	app.get("/rooms", (req, res) => res.redirect("/"));
+
+	app.get("/rooms/:id", function (req, res) {
+		if (req.params.id in entities.rooms) {
+			var room = entities.rooms[req.params.id];
+
+			res.send(tpls.room({
+				info: info,
+				room: room.id,
+				entities: room.entities.map(entity => entity.renderBox())
+			}));
+		} else {
+			res.status(404).send("No room with that ID found");
+		}
+	});
+
+	web.listen(3001);
+}
+
+backends.loadAll(function (_, datapoints) {
+	entities.loadAll(datapoints, comm, setupServer);
+});
