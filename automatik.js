91c5c6c690999d69fe37ef2cece36a223f22496c
Ole <ole@vprsm.de>
2015-10-12 17:53:48 +0200

    Bind external service instances

--- automatik.js
+++ automatik.js
@@ -1,68 +1,13 @@
-var http = require("http");
-var bodyparser = require("body-parser");
-var Express = require("express");
-
+var external = require("./lib/external");
 var tpls = require("./lib/templates");
 var backends = require("./lib/backends");
 var entities = require("./lib/entities");
 var communication = require("./lib/communication");
 
-const info = {
-	title: "automatik",
-	version: "0.0.0"
-};
-
-var app = Express();
-var web = http.Server(app);
-var comm = new communication.Communication(web);
-
-app.use(bodyparser.urlencoded({extended: true}));
-
-function setupServer() {
-	app.get("/", function (req, res) {
-		res.send(tpls.overview({
-			info: info,
-			rooms: entities.rooms
-		}));
-	});
-
-	app.get("/rooms/add", function (req, res) {
-		res.send(tpls.rooms.add({
-			info: info
-		}));
-	});
-
-	app.post("/rooms/add", function (req, res) {
-		var name = req.body.name;
-
-		if (typeof(name) == "string" && name.length > 0) {
-			entities.createRoom(name, function (room) {
-				res.redirect("/rooms/" + room.id);
-			});
-		} else {
-			res.send(tpls.rooms.add({
-				info: info,
-				error: true
-			}));
-		}
-	});
-
-	app.get("/rooms/:id", function (req, res) {
-		if (req.params.id in entities.rooms) {
-			var room = entities.rooms[req.params.id];
-
-			res.send(tpls.room({
-				info: info,
-				entities: room.entities
-			}));
-		} else {
-			res.status(404).send("No room with that ID found");
-		}
-	});
-
-	web.listen(3001);
-}
+require("./lib/routes");
 
 backends.loadAll(function () {
-	entities.loadAll(comm, setupServer);
+	entities.loadAll(communication, function () {
+		external.http.listen(3001);
+	});
 });
