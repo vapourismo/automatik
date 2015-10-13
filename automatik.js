6db4c1c0eefd709c35473e34a28e14fe6a628c57
Ole <ole@vprsm.de>
2015-10-13 15:52:59 +0200

    Simplify project struture

--- automatik.js
+++ automatik.js
@@ -1,13 +1,3 @@
-var external = require("./lib/external");
-var tpls = require("./lib/templates");
-var backends = require("./lib/backends");
-var entities = require("./lib/entities");
-var communication = require("./lib/communication");
-
+require("./lib/communication");
 require("./lib/routes");
-
-backends.loadAll(function () {
-	entities.loadAll(communication, function () {
-		external.http.listen(3001);
-	});
-});
+require("./lib/external").http.listen(3001);
