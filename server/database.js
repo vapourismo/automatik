16ad5e6857bb7d222c366037740c9809a5e9e1a5
Ole <ole@vprsm.de>
2015-10-20 19:23:34 +0200

    Add async query method

--- server/database.js
+++ server/database.js
@@ -1,7 +1,18 @@
 const pg     = require("pg");
 const config = require("./config");
 
+// Connect
 const db = new pg.Client(config.database || {});
 db.connect();
+
+// Async query method
+db.queryAsync = function (...args) {
+	return new Promise(function (accept, reject) {
+		this.query(...args, function (err, result) {
+			if (err) reject(err);
+			else     accept(result);
+		});
+	}.bind(this));
+};
 
 module.exports = db;
