25a32f0dfc4a33e64df76e6fbea9056a592f39fa
Ole <ole@vprsm.de>
2015-10-22 20:37:00 +0200

    Wrap each backend into a class instance

--- server/backends.js
+++ server/backends.js
@@ -1,27 +1,29 @@
+"use strict";
+
 const path   = require("path");
 const db     = require("./database");
 const util   = require("./utilities");
 const Driver = require("./driver");
 
 const backends = {};
-const BackendPrototype = {};
 
-function makeBackend(row) {
-	util.inform("backend: " + row.id, "Loading '" + row.name + "'");
-
-	row.__proto__ = BackendPrototype;
-	row.driver = Driver.create(row.driver, row.config);
-
-	return backends[row.id] = row;
+class Backend {
+	constructor(row) {
+		Object.assign(this, row);
+		row.driver = Driver.create(this.driver, this.config);
+	}
 }
 
 module.exports = {
 	load: function* () {
 		const backendsResult = yield db.queryAsync("SELECT * FROM backends");
-		backendsResult.rows.forEach(makeBackend);
+		backendsResult.rows.forEach(function (row) {
+			util.inform("backends", "Registering '" + row.name + "'");
+			backends[row.id] = new Backend(row);
+		});
 	}.async(),
 
-	find: function (id) {
+	find: function () {
 
 	}
 };
