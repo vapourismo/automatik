d6b7fd73cef859de8995ef47c3e322645840a645
Ole <ole@vprsm.de>
2015-10-25 00:54:15 +0200

    Add 'create' method to datapoints module

--- server/datapoints.js
+++ server/datapoints.js
@@ -8,8 +8,11 @@ const db           = require("./database");
 const datapoints = {};
 
 class Datapoint extends EventEmitter {
-	constructor() {
+	constructor(value) {
 		super();
+
+		if (value !== undefined && value !== null)
+			this.value = value;
 	}
 
 	set value(value) {
@@ -36,8 +39,16 @@ class DatapointHandle {
 		const backend = backends.find(this.backend);
 		if (!backend) throw new Error("Backend #" + this.backend + " does not exist");
 
-		this.datapoint = new Datapoint();
+		this.datapoint = new Datapoint(row.value);
 		backend.attachToDatapoint(this.config, this.datapoint);
+
+		this.datapoint.on("update", function* (value) {
+			try {
+				yield db.queryAsync("UPDATE datapints SET value = $1 WHERE id = $2", [value, row.id]);
+			} catch (error) {
+				util.error("datapoints", "Failed to update #" + row.id, error);
+			}
+		}.async);
 	}
 }
 
@@ -48,6 +59,16 @@ module.exports = {
 			util.inform("datapoints", "Registering '" + row.name + "'");
 			datapoints[row.id] = new DatapointHandle(row);
 		});
+	}.async,
+
+	create: function* (name, backend, config, value) {
+		// TODO: Validate parameters
+
+		const insertResult = yield db.queryAsync("INSERT INTO datapoints (name, backend, config, value) VALUES ($1, $2, $3, $3)", [name, backend, config, value]);
+		insertResult.rows.forEach(function (row) {
+			util.inform("datapoints", "Registering '" + row.name + "'");
+			datapoints[row.id] = new DatapointHandle(row);
+		})
 	}.async,
 
 	find: function (id) {
