f156a25f36551f35aac47d67b9ba0a256f66a3f8
Ole <ole@vprsm.de>
2015-10-31 16:03:40 +0100

    Add documentation for server-side Namespace and Channel

--- server/namespace.js
+++ server/namespace.js
@@ -1,17 +1,73 @@
 "use strict";
 
 const EventEmitter = require("events");
+const SocketIO     = require("socket.io");
 const util         = require("./utilities");
 
+/**
+ * Socket.IO channel
+ */
 class Channel {
-	constructor(name, parent) {
+	constructor(name, ns) {
 		this.name = name;
-		this.parent = parent;
-		this.namespace = parent.namespace;
+		this.namespace = ns;
 
 		this.events = new EventEmitter();
 		this.methods = {};
 	}
+
+	/**
+	 * Register a function which can be invoked by clients.
+	 * @param {String}   name     Function identifier
+	 * @param {Function} callback Function with signature `(reply, reject, ...args)` where
+	 *                            `reply` and `reject` shall be used to respond accordingly
+	 */
+	register(name, callback) {
+		this.methods[name] = callback;
+	}
+
+	/**
+	 * Trigger an event on every subscribed client.
+	 * @param {String} event Event identifier
+	 * @param {...*}   args  Event parameters
+	 */
+	trigger(event, ...args) {
+		this.send("trigger", event, ...args);
+	}
+
+	/**
+	 * Add an event handler.
+	 * @param {String}   event    Event identifier
+	 * @param {Function} callback Event handler
+	 */
+	on(event, callback) {
+		this.events.on(event, callback);
+	}
+
+	/**
+	 * Remove an event handler.
+	 * @param {String}   event    Event identifier
+	 * @param {Function} callback Event handler
+	 */
+	off(event, callback) {
+		this.events.off(event, callback);
+	}
+
+	/**
+	 * Remove this channel from the namespace. All registered functions and event handlers will be
+	 * removed.
+	 */
+	destroy() {
+		if (this.name in this.namespace.channels)
+			delete this.namespace.channels[this.name];
+
+		for (let key in this.methods)
+			delete this.methods[key];
+
+		this.events.removeAllListeners();
+	}
+
+	// Internals
 
 	attach(client) {
 		client.join(this.name);
@@ -21,38 +77,12 @@ class Channel {
 		client.leave(this.name);
 	}
 
-	register(name, callback) {
-		this.methods[name] = callback;
-	}
-
 	send(method, ...args) {
 		this.namespace.to(this.name).emit("route", this.name, method, ...args);
 	}
 
 	direct(client, method, ...args) {
 		client.emit("route", this.name, method, ...args);
-	}
-
-	trigger(event, ...args) {
-		this.send("trigger", event, ...args);
-	}
-
-	on(event, callback) {
-		this.events.on(event, callback);
-	}
-
-	off(event, callback) {
-		this.events.off(event, callback);
-	}
-
-	destroy() {
-		if (this.name in this.parent.channels)
-			delete this.parent.channels[this.name];
-
-		for (let key in this.methods)
-			delete this.methods[key];
-
-		this.events.removeAllListeners();
 	}
 
 	process(client, method, ...args) {
@@ -82,13 +112,20 @@ class Channel {
 	}
 }
 
-class Namespace {
-	constructor(ns) {
-		this.namespace = ns;
+/**
+ * Socket.IO namespace
+ */
+class Namespace extends SocketIO {
+	/**
+	 * @constructor
+	 */
+	constructor() {
+		super();
+
 		this.channels = {};
 		this.methods = {};
 
-		this.namespace.on("connection", client => {
+		this.on("connection", client => {
 			client.on("error", error => {
 				util.error("namespace", error instanceof Error ? error.stack : error);
 			});
@@ -126,6 +163,11 @@ class Namespace {
 		});
 	}
 
+	/**
+	 * Create a new Channel.
+	 * @param {String} name Channel identifier
+	 * @returns {Channel}
+	 */
 	create(name) {
 		if (name in this.channels)
 			return this.channels[name];
@@ -133,9 +175,15 @@ class Namespace {
 			return this.channels[name] = new Channel(name, this);
 	}
 
+	/**
+	 * Register a function which can be invoked by clients.
+	 * @param {String}   name     Function identifier
+	 * @param {Function} callback Function with signature `(reply, reject, ...args)` where
+	 *                            `reply` and `reject` shall be used to respond accordingly
+	 */
 	register(name, callback) {
 		this.methods[name] = callback;
 	}
 }
 
-module.exports = ns => new Namespace(ns);
+module.exports = Namespace;
